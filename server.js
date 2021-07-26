60f3ce21cae7c1fe4e507ea19dbca7389c14631a
St.Onge <stonge.ms@gmail.com>
2021-07-25 20:35:19 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
